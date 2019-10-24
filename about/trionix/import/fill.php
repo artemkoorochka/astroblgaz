769ce26d3254f6d20f4ca1dc470824b0f1b8aae7
artemkoorochka <artem@koorochka.com>
2019-10-24 19:40:35 +0300

    Prepare cron event

---
+++ about/trionix/import/fill.php
@@ -0,0 +1,12 @@
+<?
+// <editor-fold defaultstate="collapsed" desc=" # Preparato prolog">
+$_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__)."/../../..");
+$DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
+define("NO_KEEP_STATISTIC", true);
+define("NOT_CHECK_PERMISSIONS",true);
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+// </editor-fold>
+
+?>
+
+
