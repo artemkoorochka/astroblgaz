dc83316ea2f9bc22685eb340d5305f11848b79dc
artemkoorochka <artem@koorochka.com>
2019-11-02 19:55:43 +0200

    Плохо реализована задача по работе с деревом адрессов

--- bitrix/components/trionix/customer.filter/class.php
+++ bitrix/components/trionix/customer.filter/class.php
@@ -2,13 +2,32 @@
 use Bitrix\Main\Loader,
     Bitrix\Astroblgaz\AdressTable,
     Bitrix\Astroblgaz\CustomerTable,
-    Bitrix\Main\Application;
+    Bitrix\Main\Application,
+    Bitrix\Main\Localization\Loc;
 
 class customerFilter extends CBitrixComponent
 {
     private $_items;
+    private $_itemParents = array();
     private $_customers;
     private $_filter;
+    private $_errors;
+
+    /**
+     * @return mixed
+     */
+    public function getErrors()
+    {
+        return $this->_errors;
+    }
+
+    /**
+     * @param mixed $errors
+     */
+    public function setError($error)
+    {
+        $this->_errors[] = $error;
+    }
 
     /**
      * @return mixed
@@ -23,33 +42,47 @@ class customerFilter extends CBitrixComponent
      */
     public function setFilter($requestFilter)
     {
-        $strFilter = array();
-        $filter = array();
+        if(empty($requestFilter))
+            return false;
 
+        $filter = array();
         foreach ($requestFilter as $item){
-            if(is_int($item)){
+
+            if(is_array($this->_itemParents[$item]))
+            {
+                $filter = array_merge($filter, $this->_itemParents[$item]);
+            }
+
+            $filter[] = $item;
+        }
+
+        if(count($filter) > 1){
+            $requestFilter = $filter;
+            foreach ($requestFilter as $item){
+
+                if(is_array($this->_itemParents[$item]))
+                {
+                    $filter = array_merge($filter, $this->_itemParents[$item]);
+                }
+
                 $filter[] = $item;
             }
-            else{
-                $strFilter[] = $item;
-            }
         }
 
-        // get chields
-        foreach ($this->getItems() as $item){
-            if(in_array($item["PARENT"], $requestFilter)){
-                $filter[] = $item["ID"];
-            }
-        }
-        // get chields include parents
-        foreach ($this->getItems() as $item){
-            if(in_array($item["PARENT"], $filter)){
-                $filter[] = $item["ID"];
+        if(count($filter) > 1){
+            $requestFilter = $filter;
+            foreach ($requestFilter as $item){
+
+                if(is_array($this->_itemParents[$item]))
+                {
+                    $filter = array_merge($filter, $this->_itemParents[$item]);
+                }
+
+                $filter[] = $item;
             }
         }
 
         $filter = array_unique($filter);
-        $filter = array_merge($strFilter, $filter);
 
         $this->_filter = $filter;
     }
@@ -64,15 +97,54 @@ class customerFilter extends CBitrixComponent
 
     /**
      * Set customers from DB
+     * @param $adresQuery
+     * @throws \Bitrix\Main\ArgumentException
+     * @throws \Bitrix\Main\ObjectPropertyException
+     * @throws \Bitrix\Main\SystemException
      */
-    public function setCustomers()
+    public function setCustomers($adresQuery)
     {
         $customers = array();
-        $result = CustomerTable::getList();
-        while ($customer = $result->fetch()){
-            $customers[$customer["ID"]] = $customer;
+        $filter = array();
+
+        if(empty($this->getFilter())){
+            $result = CustomerTable::getList();
+        }else{
+            $result = CustomerTable::getList(array(
+                "filter" => array("PARENT" => $this->getFilter())
+            ));
         }
+
+        while ($customer = $result->fetch()){
+
+            if(!empty($this->arResult["CUSTOMERS_QUERY"]) || !empty($this->arResult["SCORE_QUERY"])){
+                if($this->arResult["CUSTOMERS_QUERY"] == $customer["ADRESS"]){
+                    $customers[$customer["ID"]] = $customer;
+                    $filter[] = $customer["ID"];
+                }
+
+                if($this->arResult["SCORE_QUERY"] == $customer["SCORE"]){
+                    $customers[$customer["ID"]] = $customer;
+                    $filter[] = $customer["ID"];
+                }
+
+            }else{
+                $filter[] = $customer["ID"];
+                $customers[$customer["ID"]] = $customer;
+
+            }
+        }
+
         $this->_customers = $customers;
+
+        if(!empty($adresQuery) && is_array($adresQuery)) {
+            if(empty($this->arResult["CUSTOMERS_QUERY"]) && empty($this->arResult["SCORE_QUERY"])){
+                //$this->_filter = array();
+                $this->setError(Loc::getMessage("ADRESS_IS_EMPTY"));
+            }
+        }
+
+
     }
 
     /**
@@ -89,10 +161,22 @@ class customerFilter extends CBitrixComponent
     public function setItems()
     {
         $items = array();
+        $_itemParents = array();
+
+        // get parent map
+        foreach ($this->getItems() as $item){
+            if($item["PARENT"] > 0)
+                $parents[$item["PARENT"]][] = $item["ID"];
+        }
 
         $result = AdressTable::getList();
         while ($item = $result->fetch()){
             $items[$item["ID"]] = $item;
+
+            // fill perents map
+            if($item["PARENT"] > 0){
+                $this->_itemParents[$item["PARENT"]][] = $item["ID"];
+            }
         }
 
         $this->_items = $items;
@@ -108,40 +192,31 @@ class customerFilter extends CBitrixComponent
         $this->_items = $separatedItems;
     }
 
+    /**
+     * @return \Bitrix\Main\HttpRequest
+     * @throws \Bitrix\Main\SystemException
+     */
     public function implementRequest(){
+        $adresQuery = "";
         if(!empty($this->getItems())){
             $request = Application::getInstance()->getContext()->getRequest();
             if($request->isPost()){
+                $adresQuery = $request->getPost($this->arParams["INPUT_NAME"]);
                 $filter = array();
-
                 if(is_array($request->getPost($this->arParams["INPUT_NAME"]))){
                     foreach ($request->getPost($this->arParams["INPUT_NAME"]) as $value){
-
-                        if(intval($value) > 0 && strlen($value) < 5){
-                            if(!empty($this->_items[$value])){
-                                $filter[] = $value;
-                                $this->_items[$value]["selected"] = "selected";
-                            }
-                        }else{
-                            if(!empty($value))
-                            {
-                                foreach ($this->getCustomers() as $item){
-
-                                    switch ($value){
-                                        case $item["ADRESS"]:
-                                                $filter[] = $value;
-                                                $this->arResult["CUSTOMERS_QUERY"] = $value;
-                                            break;
-                                        case $item["SCORE"]:
-                                            $filter[] = $value;
-                                            $this->arResult["SCORE_QUERY"] = $value;
-                                            break;
-                                    }
-
-                                }
+                        foreach ($this->getItems() as $item){
+                            if($value == $item["NAME"] . " " . $item["SHORT_NAME"] . "."){
+                                $filter[] = $item["ID"];
+                                $this->_items[$item["ID"]]["selected"] = "selected";
                             }
                         }
                     }
+
+                    $this->arResult["CUSTOMERS_QUERY"] = trim($_POST[$this->arParams["INPUT_NAME"]]["ADRESS"]);
+                    $this->arResult["SCORE_QUERY"] = trim($_POST[$this->arParams["INPUT_NAME"]]["SCORE"]);
+
+
                 }else{
                     $search = $request->getPost($this->arParams["INPUT_NAME"]);
                     $this->arResult["SEARCH_QUERY"] = $search;
@@ -164,7 +239,6 @@ class customerFilter extends CBitrixComponent
                             }
                         }
                     }
-
                 }
 
                 $this->setFilter($filter);
@@ -175,6 +249,8 @@ class customerFilter extends CBitrixComponent
                 $this->arParams["MODE"] = "full";
             }
         }
+
+        return $adresQuery;
     }
 
     public function executeComponent()
@@ -183,12 +259,12 @@ class customerFilter extends CBitrixComponent
         if($this->startResultCache()){
             if(Loader::includeModule("trionix.astroblgaz")){
                 $this->setItems();
-                $this->setCustomers();
             }
             $this->endResultCache();
         }
 
-        $this->implementRequest();
+        $adresQuery = $this->implementRequest();
+        $this->setCustomers($adresQuery);
 
         $this->arResult["ITEMS"] = $this->getItems();
         $this->arResult["CUSTOMERS"] = $this->getCustomers();
@@ -198,9 +274,12 @@ class customerFilter extends CBitrixComponent
         }
 
         $this->arResult["GROUP_ITEMS"] = $this->getItems();
+        $this->arResult["ERRORS"] = $this->getErrors();
 
         $this->includeComponentTemplate();
 
-        return $this->getFilter();
+        if(empty($this->getErrors())){
+            return $this->getFilter();
+        }
     }
 }
